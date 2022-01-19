ac8b399c751c943ee6062876112bc58ccc9c331f
WEBEDIGITAL\zhariff.hamid2020 <zhariff.hamid@webe.com.my>
2022-01-19 23:19:05 +0800

    -creates a CSV file from the string by making each character a column in the CSV and then output  "CSV created!" to stdout

--- app/Console/Commands/TestString.php
+++ app/Console/Commands/TestString.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class TestString extends Command
@@ -40,16 +41,24 @@ class TestString extends Command
         $string = $this->ask('Please key in some string here.');
         $stringArray = str_split($string);
 
-        // - converts the string to uppercase and outputs it to stdout.
+        // 1 - converts the string to uppercase and outputs it to stdout.
         $this->info(strtoupper($string));
 
-        // - converts the string to alternate upper and lower case and outputs it to stdout.
+        // 2 - converts the string to alternate upper and lower case and outputs it to stdout.
         $answer = null;
         foreach ($stringArray as $key => $eachArray) {
             $answer .= ($key % 2 == 0) ? strtoupper($eachArray) : strtolower($eachArray);
         }
         $this->info($answer);
 
-        return 0;
+        // 3 - creates a CSV file from the string by making each character a column in the CSV
+        //     and then output  "CSV created!" to stdout.
+        $date = new \DateTime();
+        $path = storage_path('app/public/'.$date->getTimestamp().'.csv');
+
+        $file = fopen($path,'w');
+        fputcsv($file, $stringArray);
+        fclose($file);
+        $this->info('CSV created!');
     }
 }
